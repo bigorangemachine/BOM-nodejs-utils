102ae9d5f3cafcb77566389d7d2e64978a409284
Brian Dinga <brian@briandinga.com>
2017-08-21 10:53:28 -0400

    Whoops restored some values

--- vars.js
+++ vars.js
@@ -4,10 +4,11 @@ var vars={
         'json':'text/json',
         'html':'text/html',
         'js':'text/javascript',
-        'js_app':'application/x-javascript'
+        'js_app':'application/javascript',
+        'x_js_app':'application/x-javascript'
     }
 };
-vars.capture_types={//files that can be captured as a text file - headers to file extension
+vars.capture_types={//files that can be captured as a text file - headers to file extension; this should be an array - check yo projects!
     'css':vars.content_types.css,
     'json':vars.content_types.json,
     'html':vars.content_types.html,
